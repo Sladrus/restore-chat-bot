017db14310cf738cecfd35f9177e3c8420fce6c2
Яков <yakov.ufimkin@gmail.com>
2024-09-09 18:49:20 +0400

    init

--- service/ChatsService.js
+++ service/ChatsService.js
@@ -1,9 +1,12 @@
+const { sleep } = require("telegram/Helpers");
 const UserBot = require("../telegram");
 
 class ChatsService {
   async getChatHistory({ chat_id, chat_url }) {
     await UserBot.joinToChat(chat_url);
+    await sleep(1000);
     const messages = await UserBot.getChatHistory(chat_id);
+    await sleep(1000);
     await UserBot.leaveChat(chat_id);
 
     return { chat_id, messages };
